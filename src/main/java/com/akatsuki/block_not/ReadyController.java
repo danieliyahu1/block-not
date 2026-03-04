5212fc8a607da19d789ed95c3db8d2d2eea11395
danieliyahu1 <danieliyahu1@gmail.com>
2026-03-04 17:35:03 +0200

    updating the endpoint to root

--- src/main/java/com/akatsuki/block_not/ReadyController.java
+++ src/main/java/com/akatsuki/block_not/ReadyController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ReadyController {
 
-	@GetMapping("/ready")
+	@GetMapping("/")
 	public String ready() {
 		return "I'm ready";
 	}
